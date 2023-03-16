823fb3d8f90e0e2258df66def3288e06942b901a
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-03-15 21:43:39 -0300

    feat: 150323

---
+++ C#/ApendendoClasses/ApendendoClasses/Lente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace ApendendoClasses
+{
+    internal class Lente
+    {
+        public float grau;
+        public String tipoFoco;
+        public String formato;
+        public bool antiReflexo;
+
+        public void exibeDados()
+        {
+            Console.WriteLine($"Grau: {grau}, Tipo de Foco: {tipoFoco}, Formato {formato}");
+        }
+    }
+}
---
+++ C#/ApendendoClasses/ApendendoClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using ApendendoClasses;
+
+Console.WriteLine("Hello, World!");
+
+Lente Lente = new Lente();
+
+Lente.grau = 1f;
+Lente.tipoFoco = "Multi-Focal";
+Lente.formato = "Quadrado";
+Lente.antiReflexo = false;
+
+Lente.exibeDados();
